5a75a468644a82a555f2c10c0830d2af86e12da1
srisrioptum@gmail.com <srisrioptum@gmail.com>
2023-07-01 13:54:48 +0000

    ok

--- models/Airline/airline.sql
+++ models/Airline/airline.sql
@@ -1,3 +1,2 @@
-{{ config(materialized='table', transient=true) }}
-
-select * from RAW.AIRLINE_DATA
+{{ config(materialized='table', transient=false) }}
+with air as (select count(*) from RAW.AIRLINE_DATA) select * from air
